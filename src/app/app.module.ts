450fa45530e7bbf40aaa8cab9ef200eb330ea00d
Gerald Michelant <gerald.michelant@gmail.com>
2017-10-02 00:11:19 -0400

    miseenpageinitiale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MdButtonModule, MdToolbarModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import 'hammerjs';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ChoresListComponent } from './chores-list/chores-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ChoresListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MdButtonModule,
+    MdToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
