3748a2ba722f662a307445290bab82c1d8b82e25
Gerald Michelant <gerald.michelant@gmail.com>
2017-10-04 11:29:01 -0400

    home route switched to liste

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -3,9 +3,8 @@ import { Routes } from '@angular/router';
 import { ListeComponent } from '../liste/liste.component';
 import { CarteComponent } from '../carte/carte.component';
 
-
 export const routes: Routes = [
   { path: 'liste',  component: ListeComponent },
   { path: 'carte',     component: CarteComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: '', redirectTo: '/liste', pathMatch: 'full' }
 ];
